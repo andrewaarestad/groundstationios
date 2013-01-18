30dc53464c4c1930e2ac63f7ba3c0aa92ab38b6b
Bryan Galusha <bryan@etal-andothers.com>
2013-01-17 19:28:12 -0800

    Added enumerations to Heartbeat message.

--- gsDemo/Classes/MAVLink/MAVHeartbeat.h
+++ gsDemo/Classes/MAVLink/MAVHeartbeat.h
@@ -9,8 +9,17 @@
 #import <Foundation/Foundation.h>
 #import "MAVMessage.h"
 #import "MAVMessageTypes.h"
+#import "mavlink_types.h"
+#import "common.h"
 
 @interface MAVHeartbeat : MAVMessage
+
+@property (nonatomic) uint32_t custom_mode;
+@property (nonatomic) enum MAV_TYPE type;
+@property (nonatomic) enum MAV_AUTOPILOT autopilot;
+@property (nonatomic) enum MAV_MODE_FLAG base_mode;
+@property (nonatomic) enum MAV_STATE system_status;
+@property (nonatomic, strong) NSNumber *mavlink_version;
 
 +(MAVHeartbeat *)initWithMAVMsg:(MAVMessage *) mavMsg;
 
